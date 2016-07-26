f4c5743677ff4b9bb6cc21a1f95b845dc5fe9917
GuiguiMtl <guillaume.delignieres@gmail.com>
2016-07-25 22:25:26 -0400

    Create project. Try to add w3 css with webpack

---
+++ LearnShareCar/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w3-top">
+        <ul class="w3-navbar w3-theme-d2 w3-left-align w3-large">
+            <li class="w3-hide-small w3-dropdown-hover">
+                <a href="#" class="w3-padding-large w3-hover-white" title="Notifications">Administration</span></a>
+                <div class="w3-dropdown-content w3-white w3-card-4">
+                    <a href="#">add a new car</a>
+                    <a href="#">Add new availibility</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
